56222687c2cfccc16564a1adbd3693effb996f3f
vadikghetta <helloiamvadik@mail.ru>
2024-10-26 19:13:52 +0300

    add logger and minimal DI

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express , {Express} from "express";
 import { userRouter } from "./users/users";
 import {Server} from "node:http";
+import LoggerService from "./logger/logger.service";
 
 export class App {
     app : Express;
     port : number;
     server : Server
+    logger : LoggerService
 
-    constructor() {
+    constructor(logger : LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     }
 
     useRoutes () {
@@ -18,6 +21,6 @@ export class App {
     public async init(){
         this.useRoutes();
         this.server = this.app.listen(this.port);
-        console.log(`Server start on http://localhost:${this.port}`)
+        this.logger.log(`Server start on http://localhost:${this.port}`)
     }
  }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,18 @@
+import { Logger, ILogObj } from "tslog";
+
+
+export default class LoggerService {
+    public logger : Logger<ILogObj>;
+    constructor () {
+        this.logger = new Logger({});
+    }
+    log(...args : unknown[]) {
+        this.logger.info(...args)
+    }
+    error(...args : unknown[]) {
+        this.logger.error(...args)
+    }
+    warn(...args : unknown[]) {
+        this.logger.warn(...args)
+    }
+}
