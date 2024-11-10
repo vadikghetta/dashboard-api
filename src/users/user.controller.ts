0917de069d20aee2741c85870d6fd986071f3d32
vadikghetta <helloiamvadik@mail.ru>
2024-11-10 22:17:15 +0300

    add interafce ti user controller

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -5,21 +5,20 @@ import { inject, injectable } from "inversify";
 import { TYPES } from "../types";
 import { ILoogerService } from "../logger/logger.interface";
 import "reflect-metadata";
+import { IUserController } from "./users.controller.interface";
 
 
 @injectable()
-export class UserController extends BaseController {
+export class UserController extends BaseController implements IUserController {
     constructor(@inject(TYPES.ILoggerService) private loggerService : ILoogerService) {
         super(loggerService)
         this.bindRoutes([
             {path : "/register" , method : "post" , func : this.register},
             {path : "/login" , method : "post" , func : this.login},
-
         ])
     }
     login(req : Request, res : Response, next : NextFunction) {
         this.ok(res, "login")
-        // next(new HttpError(401 , "no auth", "login"))
     }
     register(req : Request, res : Response, next : NextFunction) {
         this.ok(res, "register")
