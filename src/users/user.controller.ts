524ab0d71c9915e2d9ef5f663455f1db8b926fc9
vadikghetta <helloiamvadik@mail.ru>
2025-02-16 21:18:32 +0300

    feat : add user entity

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -9,6 +9,7 @@ import "reflect-metadata";
 import { IUserController } from "./users.controller.interface";
 import { UserLoginDto } from "./dto/user-login.dto";
 import { UserRegisterDto } from "./dto/user-register.dto";
+import { User } from "./user.entity";
 
 
 @injectable()
@@ -25,8 +26,9 @@ export class UserController extends BaseController implements IUserController {
 		console.log(req.body);
 		this.ok(res, "login");
 	}
-	register(req : Request<unknown, unknown, UserRegisterDto>, res : Response, next : NextFunction) : void {
-		console.log(req.body);
+	async register({body} : Request<unknown, unknown, UserRegisterDto>, res : Response, next : NextFunction) : Promise<void> {
+		const newUser = new User(body.email, body.name);
+		await newUser.setPassword(body.password);
 		this.ok(res, "register");
 	}
 
